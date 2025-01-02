528631d7b3050c1b2bf34c384cf39e0655a1d3ca
Dhananjay Sonar <82250667+dhananjay8482@users.noreply.github.com>
2025-01-02 19:09:50 +0530

    [+] Mongo DB Connected [+] User Schema and Login Signup implemented

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const express = require('express');
 
+const userRoute = require('./routes/user');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 8000;
+
+// setting Mongo DB
+mongoose.connect('mongodb://localhost:27017/blogify').then((e)=> console.log('MongoDB Connected. '))
 
 // Setting Middlewares
 app.set('view engine', 'ejs')
 app.set('views', path.resolve("./views"));
 
+// middlleware to get form data values from FE
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res)=>{
     res.render('home');
 })
+
+app.use('/user', userRoute)
 
 // Running Server
 app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`));
---
+++ models/user.js
@@ -0,0 +1,65 @@
+const {Schema, model} = require('mongoose');
+const {createHmac, randomBytes, hex} = require("crypto");
+const { create } = require('domain');
+
+const userSchema = new Schema({
+    fullName:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt:{
+        type: String,
+        // required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    profileImageURL :{
+        type: String,
+        default: '/images.default.png'
+    },
+    enum: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    }
+}, {timestamps: true})
+
+userSchema.pre("save", function(next){
+    const user = this;
+    if(!user.isModified('password')) return;
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt).update(user.password).digest(hex);
+
+    this.salt = salt;
+    this.password = hashedPassword;
+    next();
+})
+
+// virtual function to check hashed passowrd and user main passsword
+userSchema.static("matchPassword", async function(email, password){
+    const user = await this.findOne({email});
+    console.log('insided match ', user)
+    if(!user) throw new Error('User not found!');
+    
+    const salt = user.salt;
+    const hashedPassword = user.password;
+    
+    const userProvidedHashed = createHmac("sha256", salt)
+    .update(password)
+    .digest(hex);
+    
+    if(hashedPassword!=userProvidedHashed) throw new Error('Incorrect password');
+    return {...user, password: undefined, salt: undefined} ;
+} )
+
+
+const User = model("user", userSchema);
+
+module.exports = User;
