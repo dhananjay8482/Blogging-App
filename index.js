19abf9e734d4b29120fd7e5cd3ee9ef681139891
Dhananjay Sonar <82250667+dhananjay8482@users.noreply.github.com>
2025-01-04 19:14:11 +0530

    [+] Blog Schema Added and Integrated POST api

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const Blog = require('./models/blog')
+
 const mongoose = require('mongoose');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
@@ -20,14 +24,18 @@ app.set('views', path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve('./public'))) // TO SERVE PUBLIC FOLDER IMAGES STATICALLY
 
-app.get('/', (req, res)=>{
+app.get('/', async (req, res)=>{
+    const allBlogs = await Blog.find({})
     res.render('home', {
-        user: req.user
+        user: req.user, 
+        blogs: allBlogs
     });
 })
 
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
 // Running Server
 app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`));
