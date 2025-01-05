a62fc3d9dab799359760c5aa2142dc24915d70c0
Dhananjay Sonar <82250667+dhananjay8482@users.noreply.github.com>
2025-01-05 19:38:53 +0530

    [+] Add Blog Page Completed

--- routes/blog.js
+++ routes/blog.js
@@ -22,9 +22,17 @@ router.get('/add-new', (req,res)=>{
     })
 } )
 
+router.get('/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy"); 
+    console.log('blog', blog)
+    return res.render("blog",{
+        user: req.user,
+        blog
+    })
+} )
+
 router.post('/', upload.single('coverImage') , async (req,res)=>{
     const { title, body } = req.body;
-    console.log(req.body);
     const blog = await Blog.create({
         body,
         title,
